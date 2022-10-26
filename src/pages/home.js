44641c583204da72adb21c95c0bee98dec966d39
tundeblockchain <114034197+tundeblockchain@users.noreply.github.com>
2022-10-26 23:11:45 +0100

    Check connection to Polygon Testnet

--- src/pages/home.js
+++ src/pages/home.js
@@ -18,85 +18,111 @@ const Home = () => {
     const [albums, setAlbums] = useState([]);
     const [value, setValue] = useState('initial');
     const [loadingState, setLoadingState] = useState('not-loaded');
+    const [isCorrectNetwork, setIsCorrectNetwork] = useState(false);
 
     useEffect(() => {
-        loadNFTs();
+        loadNFTs(); 
     }, [value]);
+
+    const isPolygonTestnet = async () => {
+        const chainId = 80001 // Polygon Testnet
+
+        if (window.ethereum.networkVersion != chainId) {
+            console.log(window.ethereum.networkVersion)
+            return false;
+        }
+
+        return true;
+    }
 
     // Load all NFT albums on the home screen
     async function loadNFTs() {
+        const web3Modal = new Web3Modal();
+        web3Modal.clearCachedProvider();
+        const connection = await web3Modal.connect();
+        let correctNetwork = await isPolygonTestnet();
+        console.log(correctNetwork);
+        setIsCorrectNetwork(correctNetwork);
         const provider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.maticvigil.com');
         const marketContract = new ethers.Contract(process.env.REACT_APP_NFTMARKETADDRESS, Market.abi, provider);
         try {
-          const data = await marketContract.fetchAlbums();
-          console.log(data);
-          const items = await Promise.all(data.map(async i => {
+            const data = await marketContract.fetchAlbums();
+            const items = await Promise.all(data.map(async i => {
             let item = {
-              image: i.albumCoverURI,
-              title: i.albumName,
-              contract: i.nftContract
+                image: i.albumCoverURI,
+                title: i.albumName,
+                contract: i.nftContract
             }
             return item;
-          }))
-          
-          setAlbums(items);
-          console.log(albums);
-          setLoadingState('loaded');
+            }))
+            
+            setAlbums(items);
+            console.log(items)
+            setLoadingState('loaded');
         }catch(err){
-          console.log(err);
+            console.log(err);
         }
-        
       }
 
     return (
-        <Tabs>
-            <TabPane tab="FEATURED" key="1">
-                <h1 className='featuredTitle'>Today Is The Day</h1>
-                <div className='albums'>
-                    {albums.map((e) => (
-                        <Link to='/album' state={e} className='albumSelection'>
-                            <img
-                                src={e.image}
-                                alt='bull'
-                                style={{ width: '150px', marginBottom: '10px'}}
-                            ></img>
-                            <p>{e.title}</p>
-                        </Link>
-                    ))}
-                </div>
-            </TabPane>
-            <TabPane tab="GENRES" key="2">
-                <h1 className='featuredTitle'>Pop Hits</h1>
-                <div className='albums'>
-                    {albums.slice(7,13).map((e) => (
-                        <Link to='/album' state={e} className='albumSelection'>
-                            <img
-                                src={e.image}
-                                alt='bull'
-                                style={{ width: '150px', marginBottom: '10px'}}
-                            ></img>
-                            <p>{e.title}</p>
-                        </Link>
-                    ))}
-                </div>
-            </TabPane>
-            <TabPane tab="NEW RELEASES" key="3">
-                <h1 className='featuredTitle'>Classics</h1>
-                <div className='albums'>
-                    {albums.slice(0,6
-                    ).map((e) => (
-                        <Link to='/album' state={e} className='albumSelection'>
-                            <img
-                                src={e.image}
-                                alt='bull'
-                                style={{ width: '150px', marginBottom: '10px'}}
-                            ></img>
-                            <p>{e.title}</p>
-                        </Link>
-                    ))}
-                </div>
-            </TabPane>
-        </Tabs>
+        <>
+            {!isCorrectNetwork && 
+                <h1 className='featuredTitle'>Please Connect to Polygon Testnet</h1>
+            }
+            {isCorrectNetwork && 
+                <Tabs>
+                    <TabPane tab="FEATURED" key="1">
+                        <h1 className='featuredTitle'>Today Is The Day</h1>
+                        <div className='albums'>
+                            {albums.map((e) => (
+                                <Link to='/album' state={e} className='albumSelection'>
+                                    <img
+                                        src={e.image}
+                                        alt='bull'
+                                        style={{ width: '150px', marginBottom: '10px'}}
+                                    ></img>
+                                    <p>{e.title}</p>
+                                </Link>
+                            ))}
+                        </div>
+                    </TabPane>
+                    <TabPane tab="GENRES" key="2">
+                        <h1 className='featuredTitle'>Pop Hits</h1>
+                        <div className='albums'>
+                            {albums.slice(7,13).map((e) => (
+                                <Link to='/album' state={e} className='albumSelection'>
+                                    <img
+                                        src={e.image}
+                                        alt='bull'
+                                        style={{ width: '150px', marginBottom: '10px'}}
+                                    ></img>
+                                    <p>{e.title}</p>
+                                </Link>
+                            ))}
+                        </div>
+                    </TabPane>
+                    <TabPane tab="NEW RELEASES" key="3">
+                        <h1 className='featuredTitle'>Classics</h1>
+                        <div className='albums'>
+                            {albums.slice(0,6
+                            ).map((e) => (
+                                <Link to='/album' state={e} className='albumSelection'>
+                                    <img
+                                        src={e.image}
+                                        alt='bull'
+                                        style={{ width: '150px', marginBottom: '10px'}}
+                                    ></img>
+                                    <p>{e.title}</p>
+                                </Link>
+                            ))}
+                        </div>
+                    </TabPane>
+                </Tabs>
+            }
+            
+        </>
+        
+        
 
     )
 }
